04bed655fcd9046245b5815ccd83c2155b2917f5
bagoviggo <bagoviggo@gmail.com>
2023-09-30 11:34:05 +0300

    Refactor 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+""" function def pascal_triangle(n): that returns a list of lists of
+    integers representing the Pascal’s triangle of n
+"""
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
