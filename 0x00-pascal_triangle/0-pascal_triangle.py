9a659e52809522ac5af69a665337965281041f42
bagoviggo <bagoviggo@gmail.com>
2023-09-30 11:29:55 +0300

    Add 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    # Initialize the result list with the first row
+    result = [[1]]
+
+    for i in range(1, n):
+        # Generate the current row based on the previous row
+        current_row = [1]  # The first element is always 1
+        previous_row = result[-1]
+
+        for j in range(1, i):
+            current_element = previous_row[j - 1] + previous_row[j]
+            current_row.append(current_element)
+
+        current_row.append(1)  # The last element is always 1
+        result.append(current_row)
+
+    return result
