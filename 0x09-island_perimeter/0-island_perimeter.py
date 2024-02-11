717357e83820c72abb4921670c79422ac9679013
bagoviggo <bagoviggo@gmail.com>
2024-02-12 01:24:28 +0300

    Add 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter.py
+~~~~~~~~~~~~~~~~~~~~
+
+Module to calculate the perimeter of an island described in a grid.
+
+Grid representation:
+    - 0 represents water
+    - 1 represents land
+    - Each cell is square, with a side length of 1
+    - Cells are connected horizontally/vertically (not diagonally).
+    - Grid is rectangular, with its width and height not exceeding 100
+    - The grid is completely surrounded by water
+    - There is only one island (or nothing).
+    - The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+
+This module contains the following function:
+    - island_perimeter(grid): Calculates the perimeter of the island described in the given grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Parameters:
+        grid (list of list of int): The grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Example:
+        grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        island_perimeter(grid)  # Output: 12
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
