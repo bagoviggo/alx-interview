49385ac199253eb78697d357fe07e0173f5732f4
bagoviggo <bagoviggo@gmail.com>
2024-02-16 00:40:20 +0300

    Added Prime Generator method

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,74 +1,47 @@
 #!/usr/bin/python3
 
+def prime_generator():
+    """
+    Generates an infinite stream of prime numbers using a generator.
+
+    Yields:
+        int: Prime numbers.
+    """
+    yield 2  # Start with the first prime number
+    primes = [2]  # Keep track of discovered prime numbers
+    num = 3  # Start checking from the next odd number
+
+    while True:
+        is_prime = all(num % p != 0 for p in primes)
+        if is_prime:
+            primes.append(num)
+            yield num
+        num += 2  # Move to the next odd number
+
 def isWinner(x, nums):
     """
     Determines the winner of the Prime Game.
 
     Args:
         x (int): Number of rounds.
-        nums (List[int]): List of integers representing
-        the set of numbers for each round.
+        nums (List[int]): List of integers representing the set of numbers for each round.
 
     Returns:
-        str or None: Name of the player who won the most rounds
-        (either "Maria" or "Ben").
+        str or None: Name of the player who won the most rounds (either "Maria" or "Ben").
                      If the winner cannot be determined, returns None.
     """
-    def is_prime(num):
-        """
-        Checks if a given number is prime.
-
-        Args:
-            num (int): The number to check.
-
-        Returns:
-            bool: True if the number is prime, False otherwise.
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def get_next_prime(nums, start):
-        """
-        Finds the next prime number in the list.
-
-        Args:
-            nums (List[int]): List of integers.
-            start (int): Index to start searching from.
-
-        Returns:
-            int or None: Next prime number or None if not found.
-        """
-        for num in nums[start:]:
-            if is_prime(num):
-                return num
-        return None
-
-    def play_round(nums):
-        """
-        Simulates a single round of the game.
-
-        Args:
-            nums (List[int]): List of integers for the current round.
-
-        Returns:
-            bool: True if Maria wins the round, False if Ben wins.
-        """
-        maria_turn = True
-        while nums:
-            next_prime = get_next_prime(nums, 0)
-            if next_prime is None:
-                break
-            nums.remove(next_prime)
-            maria_turn = not maria_turn
-        return maria_turn
-
+    prime_gen = prime_generator()
     maria_wins = 0
+
     for _ in range(x):
-        if play_round(nums.copy()):
+        maria_turn = True
+        for _ in range(nums[_]):
+            next_prime = next(prime_gen)
+            if next_prime > nums[_]:
+                break
+            maria_turn = not maria_turn
+
+        if maria_turn:
             maria_wins += 1
 
     if maria_wins > x // 2:
@@ -78,6 +51,7 @@ def isWinner(x, nums):
     else:
         return None
 
-
+# Example usage
 if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+
